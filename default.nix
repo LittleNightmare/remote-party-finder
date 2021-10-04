891f636e65344009be1e3c8639ce5b317c38eb0b
Anna Clemens <git@annaclemens.io>
2021-10-03 23:20:29 -0400

    chore: add beginnings of nix

---
+++ default.nix
@@ -0,0 +1,10 @@
+let
+    sources = import ./nix/sources.nix { };
+    pkgs = import sources.nixpkgs { overlays = [ (import sources.mozilla) ]; };
+in
+pkgs.mkShell {
+    buildInputs = [
+        (pkgs.rustChannelOf { date = "2021-09-20"; channel = "nightly"; }).rust
+        pkgs.gcc
+    ];
+}
