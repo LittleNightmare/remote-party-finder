abe38fde09e7ced0722b434a0f2472503ec5e921
LittleNightmare <littlenightmare2000@gmail.com>
2024-01-08 22:05:19 -0500

    change rust version

--- server/default.nix
+++ server/default.nix
@@ -1,7 +1,7 @@
 let
     sources = import ./nix/sources.nix { };
     pkgs = import sources.nixpkgs { overlays = [ (import sources.mozilla) ]; };
-    rust = pkgs.rustChannelOfTargets "nightly" "2022-07-04" [ "x86_64-unknown-linux-musl" ];
+    rust = pkgs.rustChannelOfTargets "nightly" "2024-01-04" [ "x86_64-unknown-linux-musl" ];
 in
 pkgs.mkShell {
     buildInputs = [
